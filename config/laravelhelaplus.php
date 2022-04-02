abbd627cf53e7e7537ae4e502723bb15342e1919
le-yo <le-yo@le-yo.com>
2022-04-02 03:59:50 +0300

    Updated config

--- config/laravelhelaplus.php
+++ config/laravelhelaplus.php
@@ -1,13 +1,11 @@
 <?php
 
 return [
-    'b2b' => [
-        'initiator' => env ('B2B_INITIATOR'),
-        'password' => env ('B2B_PASSWORD'),
-        'source' => env ('B2B_SOURCE'),
-        'result_url' => env ('B2B_RESULT_URL'),
-        'callback_url' => env ('B2B_CALLBACK_URL'),
-        'securitycredential' => env ('B2B_SECURITY_CREDENTIAL'),
-    ],
+    'b2binitiator' => env ('B2B_INITIATOR'),
+    'b2bpassword' => env ('B2B_PASSWORD'),
+    'b2bsource' => env ('B2B_SOURCE'),
+    'b2bresult_url' => env ('B2B_RESULT_URL'),
+    'b2bcallback_url' => env ('B2B_CALLBACK_URL'),
+    'b2bsecuritycredential' => env ('B2B_SECURITY_CREDENTIAL'),
     'helaplus_api_token'=>env('HELAPLUS_API_TOKEN')
-];
+]; 
