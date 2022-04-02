7c9cf749c61a48521b77db168396d938a2148692
le-yo <le-yo@le-yo.com>
2022-04-02 04:02:02 +0300

    Updated b2bsendmoney

--- config/laravelhelaplus.php
+++ config/laravelhelaplus.php
@@ -8,4 +8,4 @@ return [
     'b2bcallback_url' => env ('B2B_CALLBACK_URL'),
     'b2bsecuritycredential' => env ('B2B_SECURITY_CREDENTIAL'),
     'helaplus_api_token'=>env('HELAPLUS_API_TOKEN')
-]; 
+];
--- src/Http/B2BPaymentController.php
+++ src/Http/B2BPaymentController.php
@@ -8,21 +8,21 @@ use Illuminate\Support\Facades\Http;
 class B2BPaymentController extends Controller {
     public static function sendB2BPayment($amount,$receiver,$command,$reference){
         $data = [
-            'Initiator'=> config('laravelhelaplus.b2b.initiator'),
-            'SecurityCredential'=> config('laravelhelaplus.b2b.securitycredential'),
+            'Initiator'=> config('laravelhelaplus.b2binitiator'),
+            'SecurityCredential'=> config('laravelhelaplus.b2bsecuritycredential'),
             'CommandID'=> $command,
             'initiator_identifier_type'=> 11,
-            'PartyA'=> config('laravelhelaplus.b2b.source'),
+            'PartyA'=> config('laravelhelaplus.b2bsource'),
             'PartyB'=> $receiver,
             'Amount'=> $amount,
             'RecieverIdentifierType'=> 4,
             'SenderIdentifierType'=> 4,
-            'ResultURL'=> config('laravelhelaplus.b2b.result_url'),
-            'QueueTimeOutURL'=> config('laravelhelaplus.b2b.result_url'),
+            'ResultURL'=> config('laravelhelaplus.b2bresult_url'),
+            'QueueTimeOutURL'=> config('laravelhelaplus.b2bresult_url'),
             'Remarks'=> $reference,
             'AccountReference'=> $reference,
             'Occasion'=> $reference,
-            ];
+            ];  
 
         return Http::withToken([
             config('laravelhelaplus.helaplus_api_token')
