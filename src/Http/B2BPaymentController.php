ad8e5d86b577fd0a94b5b0ec105f6e721824a8ec
le-yo <le-yo@le-yo.com>
2022-04-02 04:11:38 +0300

    fixing an issue with config

--- src/Http/B2BPaymentController.php
+++ src/Http/B2BPaymentController.php
@@ -8,26 +8,26 @@ use Illuminate\Support\Facades\Http;
 class B2BPaymentController extends Controller {
     public static function sendB2BPayment($amount,$receiver,$command,$reference){
         $data = [
-            'Initiator'=> config('laravelhelaplus.b2binitiator'),
-            'SecurityCredential'=> config('laravelhelaplus.b2bsecuritycredential'),
+            'Initiator'=> config('laravelhelaplus.b2b.initiator'),
+            'SecurityCredential'=> config('laravelhelaplus.b2b.securitycredential'),
             'CommandID'=> $command,
             'initiator_identifier_type'=> 11,
-            'PartyA'=> config('laravelhelaplus.b2bsource'),
+            'PartyA'=> config('laravelhelaplus.b2b.source'),
             'PartyB'=> $receiver,
             'Amount'=> $amount,
             'RecieverIdentifierType'=> 4,
             'SenderIdentifierType'=> 4,
-            'ResultURL'=> config('laravelhelaplus.b2bresult_url'),
-            'QueueTimeOutURL'=> config('laravelhelaplus.b2bresult_url'),
+            'ResultURL'=> config('laravelhelaplus.b2b.result_url'),
+            'QueueTimeOutURL'=> config('laravelhelaplus.b2b.result_url'),
             'Remarks'=> $reference,
             'AccountReference'=> $reference,
             'Occasion'=> $reference,
             ];
- 
+
         return Http::withToken(
             config('laravelhelaplus.helaplus_api_token')
         )->post(config('laravelhelaplus.b2b.result_url'),$data)->body();
 
-    }
+    }  
 
 }
