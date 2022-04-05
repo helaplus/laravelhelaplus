ae806243a8b1173032975725a2a38d0d1937d02d
le-yo <le-yo@le-yo.com>
2022-04-05 04:35:39 +0300

    Added helaplus_transaction migration

---
+++ database/migrations/2022_04_05_012700_create_transactions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('helaplus_transaction', function (Blueprint $table) {
+            $table->id();
+            $table->string('type');
+            $table->string('amount');
+            $table->string('recipient');
+            $table->string('reference')->nullable();
+            $table->text('details')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('helaplus_transaction');
+    }
+};
