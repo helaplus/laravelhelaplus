80e4725a785560d841ed2f4377f5dd83b7c64b8a
le-yo <le-yo@le-yo.com>
2022-04-29 02:01:06 +0300

    added helaplusLog model and migration

---
+++ src/Models/helaplusLog.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Helaplus\Laravelhelaplus\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class helaplusLog extends Model
+{
+    use HasFactory;
+}
