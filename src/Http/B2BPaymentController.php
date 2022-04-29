302693d201615637f0ccd423fc335ca92d027e13
le-yo <le-yo@le-yo.com>
2022-04-29 04:44:44 +0300

    Fixing issues

--- src/Http/B2BPaymentController.php
+++ src/Http/B2BPaymentController.php
@@ -99,8 +99,8 @@ class B2BPaymentController extends Controller {
         $helaplusLog->payload = file_get_contents('php://input');
         $helaplusLog->save();
         $xml = new \DOMDocument();
-        $response = json_decode($helaplusLog->payload);
-        $working_account_balance = explode("Working Account|KES|",$response);
+        $response = json_decode($helaplusLog->payload); 
+        $working_account_balance = explode("Working Account|KES|",$response->data->response);
         $working_account_balance = explode("|",$working_account_balance[1]);
         return $working_account_balance;
         print_r($working_account_balance);
