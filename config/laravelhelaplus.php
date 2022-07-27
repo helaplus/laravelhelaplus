289f1068a2f3d80841744d590e1d46eefbac1387
le-yo <le-yo@le-yo.com>
2022-07-27 17:32:24 +0300

    Fixing helaplus_c2b_endpoint to helaplus_b2c_endpoint

--- config/laravelhelaplus.php
+++ config/laravelhelaplus.php
@@ -31,7 +31,7 @@ return [
         'result_url' => env ('B2C_RESULT_URL'),
         'callback_url' => env ('B2C_CALLBACK_URL'),
         'securitycredential' => env ('B2C_SECURITY_CREDENTIAL'),
-        'helaplus_c2b_endpoint' => env ('HELAPLUS_B2C_ENDPOINT'),
+        'helaplus_b2c_endpoint' => env ('HELAPLUS_B2C_ENDPOINT'), 
     ],
     'helaplus_api_token'=>env('HELAPLUS_API_TOKEN')
 ];
